640d10d5ebafb3ccd9c5b6412080f62b657133c1
SHRISHMA PANDEY <88433920+ShIrishma-Coder@users.noreply.github.com>
2022-02-01 23:42:00 +0530

    Add files via upload
    JAVA PART

---
+++ udp.java
@@ -0,0 +1,30 @@
+udpclient
+
+import java.net.*; 
+public class UDPClient{
+    public static void main(String[] args) throws Exception { 
+    DatagramSocket ds = new DatagramSocket(3000); 
+    byte[] buf = new byte[1024];
+    DatagramPacket dp = new DatagramPacket(buf, 1024); ds.receive(dp);
+    String str = new String(dp.getData(), 0, dp.getLength()); 
+    System.out.println(str);
+    ds.close();
+    } }
+
+
+udpserver
+
+import java.net.*; 
+import java.util.Scanner;
+public class UDPServer{
+public static void main(String[] args) throws Exception { 
+DatagramSocket ds = new DatagramSocket();
+String str = "";
+Scanner sc=new Scanner(System.in);
+System.out.println("\n Enter the message : ");
+str=sc.nextLine();
+InetAddress ip = InetAddress.getByName("127.0.0.1");
+DatagramPacket dp = new DatagramPacket(str.getBytes(), str.length(), ip, 3000); 
+ds.send(dp);
+ds.close();
+} }
